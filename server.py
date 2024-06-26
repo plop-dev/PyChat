857065e707464aed8de236997126d7e8081da6a7
plop <128096028+plop-dev@users.noreply.github.com>
2024-06-26 22:23:51 +0100

    finished server.py

--- server.py
+++ server.py
@@ -21,6 +21,9 @@ try:
 except socket.error as e:
     print(f"Error: Failed to bind socket: {e}")
     exit(1)
+# listen for upcoming connections
+s.listen(5)
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
 
 
 def listen_for_client(cs: socket.socket):
@@ -47,9 +50,37 @@ def listen_for_client(cs: socket.socket):
                 for client_socketd in client_sockets:
                     if client_socketd['socket'] == cs:
                         client_sockets.remove(client_socketd)
-                break
+                break  # Exit the loop if no message received
+            elif msg.__contains__('<S>disconnect'):
+                time_now = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+
+                for client_socketd in client_sockets:
+                    if client_socketd['socket'] == cs:
+                        client_socketd['last_online'] = time_now
+            elif msg.__contains__('<S>connect'):
+                for client_socketd in client_sockets:
+                    if client_socketd['socket'] == cs:
+                        client_socketd['last_online'] = "now"
+            elif msg.__contains__('<H>'):
+                for client_socketd in client_sockets:
+                    if client_socketd['socket'] == cs:
+                        client_socketd['username'] = msg.replace('<H>', '')
             else:
                 msg = msg.replace(separator_token, ": ")
+
+        # iterate over all connected sockets
+        for client_socketd in client_sockets:
+            # and send the message
+            try:
+                client_socket = client_socketd['socket']
+                client_socket.send(msg.encode())
+                if not msg.__contains__('<H>') and not msg.__contains__('<S>'):
+                    date = msg.split('|')[0]
+                    sender = msg.split('|')[1]
+                    message = msg.split('|')[2]
+                    MESSAGES.append({"date": date, "sender": sender, "message": message})
+            except Exception as e:
+                print(f"[!] Error sending message to client: {e}")
 
 
 while True:
@@ -60,6 +91,11 @@ while True:
         print(f"[!] Error accepting connection: {e}")
         continue  # Continue to the next iteration if an error occurs
 
+    
+    print(f"[+] {client_address} connected.")
+    # add the new connected client to connected sockets
+    time_now = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+    client_sockets.append({"socket": client_socket, "last_online": time_now})
     # start a new thread that listens for each client's messages
     t = Thread(target=listen_for_client, args=(client_socket,))
     # make the thread daemon so it ends whenever the main thread ends
