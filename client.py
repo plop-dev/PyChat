41f3e483befe805e4d0dbc4a167888982eb8badc
plop <128096028+plop-dev@users.noreply.github.com>
2024-06-27 20:33:27 +0100

    go client.py to work normally

--- client.py
+++ client.py
@@ -2,6 +2,18 @@ import socket
 import random
 from threading import Thread
 from datetime import datetime
+from colorama import Fore, init, Back
+from win11toast import toast
+
+# init colors
+init()
+
+# set the available colors
+colors = [Fore.BLUE, Fore.CYAN, Fore.GREEN,
+    Fore.LIGHTBLUE_EX, Fore.LIGHTCYAN_EX, Fore.LIGHTGREEN_EX, 
+    Fore.LIGHTMAGENTA_EX, Fore.LIGHTRED_EX,
+    Fore.LIGHTYELLOW_EX, Fore.MAGENTA, Fore.RED
+]
 
 # choose a random color for the client
 client_color = random.choice(colors)
@@ -25,6 +37,29 @@ name = input("Enter your name: ")
 def listen_for_messages():
     while True:
         messageRAW = s.recv(1024).decode()
+        
+        if messageRAW.__contains__('<S>'):
+            print('\n' + messageRAW.replace("<S>", ""))
+            continue
+        else:
+            date = messageRAW.split('|')[0]
+            sender = messageRAW.split('|')[1]
+            message = messageRAW.split('|')[2]
+            
+            if sender == name:
+                print(f'\n{client_color}[{date}] You: {message}{Fore.RESET}')
+            else:
+                try:
+                    print(f'\n{client_color}[{date}] {sender}: {message}{Fore.RESET}')
+                except:
+                    print('An error occured.')
+
+# make a thread that listens for messages to this client & print them
+t = Thread(target=listen_for_messages)
+# make the thread daemon so it ends whenever the main thread ends
+t.daemon = True
+# start the thread
+t.start()
 
 def send(to_send):
     time_now = datetime.now().strftime('%H:%M:%S')
@@ -41,4 +76,6 @@ while True:
     elif to_send.lower().strip() != '':
         send(to_send)
 
+# close the socket
+s.send(f'<S>[-] {client_color}{name} disconnected{Fore.RESET}'.encode())
 s.close()
